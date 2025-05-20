a91d2c6b515cf487e8821a5c66aef65f118d94d8
Prashansa <146424970+Prashansa-Chaurasia@users.noreply.github.com>
2025-05-20 08:50:29 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,38 @@
+
+
+$(".name").hover(function(){
+    $(".name").css("font-size","100px");
+
+  },
+  function(){
+    $(".name").css("font-size","80px");
+  }); 
+
+  $(".dicelink").hover(function(){
+    $(".dicelink").css("font-size","120%");
+
+  },
+  function(){
+    $(".dicelink").css("font-size","100%");
+  });
+
+  $(".drumlink").hover(function(){
+    $(".drumlink").css("font-size","120%");
+
+  },
+  function(){
+    $(".drumlink").css("font-size","100%");
+  }); 
+
+  $(".tinlink").hover(function(){
+    $(".tinlink").css("font-size","120%");
+
+  },
+  function(){
+    $(".tinlink").css("font-size","100%");
+  }); 
+
+
+
+
+  
